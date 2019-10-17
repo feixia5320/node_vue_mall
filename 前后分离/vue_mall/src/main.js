1a8e253494406d66e316035b68efe1ee707d423e
feixia5320 <1044489176@qq.com>
2019-10-17 12:20:42 +0800

    引入axios，跨域访问接口

--- 前后分离/vue_mall/src/main.js
+++ 前后分离/vue_mall/src/main.js
@@ -3,13 +3,49 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import axios from 'axios'
+import Vuex from 'vuex'
 
+Vue.use(Vuex);
 Vue.config.productionTip = false
 
+const store = new Vuex.Store({
+  state: {
+    nickName:'',
+    cartCount:0
+  },
+  mutations: {
+    //更新用户信息
+    updateUserInfo(state, nickName) {
+      state.nickName = nickName;
+    },
+    updateCartCount(state,cartCount){
+      state.cartCount += cartCount;
+    }
+  }
+});
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
+  mounted(){
+    this.checkLogin();
+  },
+  methods:{
+    checkLogin(){
+      axios.get("users/checkLogin").then(res=> {
+        var res = res.data;
+        if (res.status == "0") {
+          this.$store.commit("updateUserInfo", res.result);
+        }else{
+          if(this.$route.path!="/goods"){
+            this.$router.push("/goods");
+          }
+        }
+      });
+    },
+  },
   components: { App },
   template: '<App/>'
 })
